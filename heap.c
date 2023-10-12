90b65bda02727561898588c4096a4279c06dd377
Replit user <>
2023-10-12 14:43:15 +0000

    new try

--- heap.c
+++ heap.c
@@ -1,41 +1,31 @@
+#include "heap.h"
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <ctype.h>
-#include "heap.h"
 
-typedef struct nodo{
-   void* data;
-   int priority;
-}heapElem;
+typedef struct nodo {
+  void *data;
+  int priority;
+} heapElem;
 
-typedef struct Heap{
-  heapElem* heapArray;
+typedef struct Heap {
+  heapElem *heapArray;
   int size;
   int capac;
 } Heap;
 
+void *heap_top(Heap *pq) { return pq->heapArray[0].data; }
 
-void* heap_top(Heap* pq){
-    return NULL;
-}
+void heap_push(Heap *pq, void *data, int priority) {}
 
+void heap_pop(Heap *pq) {}
 
-
-void heap_push(Heap* pq, void* data, int priority){
-
-}
-
-
-void heap_pop(Heap* pq){
-
-}
-
-Heap* createHeap(){
-  Heap *heap = (Heap*)malloc(sizeof(Heap));
+Heap *createHeap() {
+  Heap *heap = (Heap *)malloc(sizeof(Heap));
   heap->size = 0;
   heap->capac = 3;
-  heap->heapArray = (heapElem*)malloc(3 * sizeof(heapElem));
-   return heap;
+  heap->heapArray = (heapElem *)malloc(3 * sizeof(heapElem));
+  return heap;
 }
